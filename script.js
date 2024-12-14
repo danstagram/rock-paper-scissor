a43cbb9a29f6fec940663cb6e3e57a0ceab428ca
Dan <101015168+danstagram@users.noreply.github.com>
2024-12-14 23:55:35 +0100

    Changing the Code to allow Clicking the selection

--- script.js
+++ script.js
@@ -69,46 +69,84 @@ function playRound(humanChoice, computerChoice){
 
 console.clear();
 
+let restultsText = ""
+
 if (winner === "human"){
     humanScore++;
     console.warn("The winner is " + winner);
+    restultsText = "The winner is " + winner;
 
 }else if (winner === "computer"){
     computerScore++;
     console.warn("The winner is " + winner);
+    restultsText = "The winner is " + winner;
+
 
 } else {
     console.warn("It's a tie!");
+    restultsText = "It's a " + winner;
 
 }
     console.log("Human selected " + humanSelection);
     console.log("Computer selected " + computerSelection);
     console.log("Score Human: " + humanScore);
     console.log("Score Computer: " + computerScore);
-}
 
 
-function playGame(){
-
-    //reset count variables
-    humanScore = 0;
-    computerScore = 0;
-    
-    playRound();
-
-    console.clear();
-
-    if (humanScore > computerScore){
-        console.warn("Human wins!");
-    } else if (computerScore > humanScore){
-        console.warn("Computer wins!");
+    //check if computer selection is already displayed
+    let element_check = document.getElementById("computerExplain");
+    let element_check2 = document.getElementById("computerOutput");
+    if (element_check !== null){    
+        element_check.remove(); 
+        element_check2.remove();
     } else {
-        console.warn("It's a tie!");
     }
 
-    console.log("Final Score:");
-    console.log("Score Human: "+humanScore);
-    console.log("Score Computer: "+computerScore);
+
+    //show computer selection
+    const game_box = document.querySelector(".game");
+    const computer_explain = document.createElement("div");
+    const computer_output = document.createElement("button");
+    game_box.appendChild(computer_explain);
+    computer_explain.textContent = "Computer selected: "+computerSelection;
+    computer_explain.id = "computerExplain";
+    game_box.appendChild(computer_output);
+    computer_output.id = "computerOutput";
+    computer_output.textContent = restultsText;
+    computer_output.style.background = "#0B192C";
+    computer_output.style.borderRadius = "2em";
+    computer_output.style.padding = "1em";
+
+
+    //update scoreboard
+    const scoreboard_human = document.querySelector(".score-human-counter");
+    const scoreboard_computer = document.querySelector(".score-computer-counter");
+
+    scoreboard_human.innerHTML = humanScore;
+    scoreboard_computer.innerHTML = computerScore;
+
+
+    //end game when one player score 5 points
+    let winner_round = "";
+    if (humanScore == 5){
+        winner_round = "human";
+    }
+    else if (computerScore == 5){
+        winner_round = "computer";
+    }
+
+    if (humanScore == 5 || computerScore == 5){
+        console.log("END OF GAME; HUMAN WINS");
+        const game_area = document.querySelector(".game");
+        const game_area1 = document.querySelector(".game-title");
+        const game_area2 = document.querySelector(".game-move-box");
+        const end_game_message = document.createElement("div");
+        game_area1.remove();
+        game_area2.remove();
+        game_area.appendChild(end_game_message);
+        game_area.id ="endOfGame";
+        game_area.textContent = "End of Game. Winner is " + winner_round + ".";
+    }
 }
 
 //change button color when mouseover:
